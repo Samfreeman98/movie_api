0e50714f90d453d08cb7678b67173bda71ad1e5b
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-10-25 22:07:41 -0600

    Add topHorrorMovies

---
+++ index.js
@@ -0,0 +1,57 @@
+let topHorrorMovies = [
+  {
+    Title: "Scream 5",
+    Director: "Matt Bettinelli-Opin, Tyler Gillett",
+  },
+
+  {
+    Title: "The Conjuring 2",
+    Director: "James Wan",
+  },
+
+  {
+    Title: "Annabelle",
+    Director: "John R. Leonetti",
+  },
+
+  {
+    Title: "Insidious",
+    Director: "James Wan",
+  },
+
+  {
+    Title: "IT",
+    Director: "Andy Muschietti",
+  },
+
+  {
+    Title: "A Nightmare on Elm Street",
+    Director: "Samuel Bayer",
+  },
+
+  {
+    Title: "The Nun",
+    Director: "Corin Hardy",
+  },
+
+  {
+    Title: "The Meg",
+    Director: "Jon Turteltaub",
+  },
+
+  {
+    Title: "Smile",
+    Director: "Parker Finn",
+  },
+
+  {
+    Title: "The Curese of La Llorona",
+    Director: "Michael Chaves",
+  },
+
+  {
+    Title: "The ring",
+    Director: "Gore Verbinski",
+  },
+];
+
