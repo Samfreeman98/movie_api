2af578d35d0cf66b67f90838bf4894611a7f7e48
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-11-01 21:35:29 -0600

    Create users

--- index.js
+++ index.js
@@ -102,6 +102,19 @@ let movies = [
   }},
 ];
 
+//CREATE
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser); 
+  } else {
+    res.status(400).send("users need name")
+  }
+})
+
 //sets up the logger
 app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.static("public"));
