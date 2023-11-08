d1a8b2bf90b6a07447b7594ddd798b7c2b62be6c
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-11-07 20:11:58 -0700

    Add genres and endpoint for genres

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let users = [
   {
     id: 1,
     name: "Billy",
-    favoriteMovies: [],
+    favoriteMovies: ["Insidious"],
   },
   {
     id: 2,
@@ -39,155 +39,97 @@ let movies = [
     "Title":"Scream 5",
     "Director": {
       "Name":"Matt Bettinelli-Opin, Tyler Gillett",
-  }},
+  }
+
+},
 
   {
     "Title":"The Conjuring 2",
     "Director":{
       "Name": "James Wan",
-  }},
+  }
+
+},
 
   {
     "Title":"Annabelle",
     "Director": {
       "Name": "John R. Leonetti",
-  }},
+  }
+
+},
 
   {
     "Title":"Insidious",
+    "Genre": "Slasher",
     "Director": {
      "Name": "James Wan",
   }},
 
   {
     "Title":"IT",
+    "Genre": "Suspense",
     "Director": {
      "Name": "Andy Muschietti",
   }},
 
   {
     "Title":"A Nightmare on Elm Street",
+    "Genre": "Slasher",
     "Director": {
       "name":"Samuel Bayer",
   }},
 
   {
     "Title":"The Nun",
+    "Genre":"Drama",
     "Director": {
      "Name": "Corin Hardy",
   }},
 
   {
     "Title":"The Meg",
+    "Genre":"Action",
     "Director": {
     "Name":"Jon Turteltaub",
   }},
 
   {
     "Title":"Smile",
+    "Genre":"Suspense",
     "Director": {
      "Name": "Parker Finn",
   }},
 
   {
     "Title":"The Curese of La Llorona",
+    "Genre":"Drama",
     "Director": {
      "Name": "Michael Chaves",
   }},
 
   {
     "Title":"The ring",
+    "Genre":"Thriller",
     "Director": {
      "Name": "Gore Verbinski",
   }},
 ];
-
-//CREATE
-app.post("/users", (req, res) => {
-  const newUser = req.body;
-
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser); 
-  } else {
-    res.status(400).send("users need name")
-  }
-})
-
-//UPDATE
-app.put("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const updatedUser = req.body;
-
-  let user = users.find( user => user.id == id);
-
-  if (user){
-    user.name = updatedUser.name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send("no such user");
-  }
-})
-
-//CREATE
-app.post("/users/:id/:movieTitle", (req, res) => {
-  const { id, movieTitle } = req.params;
-
-  let user = users.find( user => user.id == id);
-
-  if (user){
-    user.favoriteMovies.push(movieTitle);
-    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);;
-  } else {
-    res.status(400).send("no such user");
-  }
-})
-
-//DELETE
-app.delete("/users/:id/:movieTitle", (req, res) => {
-  const { id, movieTitle } = req.params;
-
-  let user = users.find( user => user.id == id);
-
-  if (user){
-    user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
-    res.status(200).send(`${movieTitle} has been removed fron user ${id}'s array`);;
-  } else {
-    res.status(400).send("no such user");
-  }
-})
-
-//DELETE
-app.delete("/users/:id", (req, res) => {
-  const { id } = req.params;
-
-  let user = users.find( user => user.id == id);
-
-  if (user){
-    users = users.filter( user => user.id == id);
-    res.status(200).send(`user ${id} has been deleted`);;
-  } else {
-    res.status(400).send("no such user");
-  }
-})
 
 //sets up the logger
 app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
-//READ
+
 app.get("/movies", (req, res) => {
   res.status(200).json(movies);
 });
 
-//READ
 app.get("/users", (req, res) => {
   res.status(200).json(users);
 });
 
-//READ
 app.get("/movies/:title", (req, res) => {
   const { title } = req.params;
   const movie = movies.find( movie => movie.Title === title);
@@ -200,10 +142,9 @@ app.get("/movies/:title", (req, res) => {
 
 });
 
-//READ
-app.get("/movies/director/:directorName", (req, res) => {
+app.get("/movies/:directorName", (req, res) => {
   const { directorName } = req.params;
-  const director = movies.find(movie => movie.Director.Name === directorName).Director;
+  const director = movies.find(movie => movie.director === directorName);
   
   if (director) {
     res.status(200).json(director);
@@ -212,12 +153,21 @@ app.get("/movies/director/:directorName", (req, res) => {
   }
 });
 
-//READ
+app.get("/movies/:genreName", (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find(movie => movie.genre === genreName);
+  
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send("no such genre")
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("What's your favorite scary movie?");
 });
 
-//READ
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
