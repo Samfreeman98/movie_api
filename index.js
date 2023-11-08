8b662d69e291dd22b8b4fb31b687264dac76c49b
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-11-07 21:54:35 -0700

    Add update

--- index.js
+++ index.js
@@ -132,6 +132,21 @@ app.post("/users", (req, res) => {
   }
 })
 
+//UPDATE
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find( user => user.id == id);
+
+  if (user){
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("no such user");
+  }
+})
+
 //DELETE
 app.delete("/users/:id/:movieTitle", (req, res) => {
   const { id, movieTitle } = req.params;
