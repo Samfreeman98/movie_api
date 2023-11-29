ad1191f3e7a32677c1f27f85b9e10a29ef47563c
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-11-28 21:25:32 -0700

    READ: user by username

--- index.js
+++ index.js
@@ -179,6 +179,18 @@ app.get("/users", async (req, res) => {
   });
 });
 
+//READ get user by username
+app.get("/users/:Username", async (req,res) => {
+  await Users.findOne({ Username: req.params.Username})
+  .then((user) => {
+    res.json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
+
 });
 
 app.get("/", (req, res) => {
