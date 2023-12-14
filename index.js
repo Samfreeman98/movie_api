6fb44c0c39b156494270958221be11487e051afc
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-12-13 21:52:35 -0700

    Fix: remove quotes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { check, validationResult } = require("express-validator");
 
 const app = express();
 
-mongoose.connect("process.env.CONNECTION_URI", { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let userSchema = mongoose.Schema({
   Username: {type: String, required: true},
