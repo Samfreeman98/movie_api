ddd9eec6f8c76563ff5ce05e6abc4378f8486d3b
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2024-01-16 21:38:43 -0700

    Add express.static

--- index.js
+++ index.js
@@ -282,6 +282,8 @@ app.get("/movies/genre/:Genre", passport.authenticate("jwt", { session: false })
   });
 });
 
+app.use(express.static("public"));
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
