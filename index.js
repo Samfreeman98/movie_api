f2115aeed07e172240b23c7a712be6e0a09778e2
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2024-01-24 19:34:43 -0700

    Update from combined to common

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cors({
 
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(morgan("combined"));
+app.use(morgan("common"));
 
 let auth = require("./auth")(app);
 const passport = require("passport");
