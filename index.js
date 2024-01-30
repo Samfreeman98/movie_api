c1b85f33c461385b3fb3a3ae7a413cf2699b6cb7
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2024-01-29 20:38:13 -0700

    Fix: add missed syntax

--- index.js
+++ index.js
@@ -164,7 +164,8 @@ app.put("/users/:Username", passport.authenticate("jwt", { session: false }),
     })
     .catch((err) => {
       console.error(err);
-    });
+  })
+});
 
 
 //DELETE movie from user's favorite movies
