e180da8e8f7d41c0d94b49baf9b018a9a11850b7
Samantha <140463087+Samfreeman98@users.noreply.github.com>
2024-02-25 19:51:30 -0700

    Remove passport.authenticate from the get /movies endpoint

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ app.delete('/users/:Username', passport.authenticate("jwt", { session: false }),
 });
 
 //READ get all movies
-app.get("/movies", passport.authenticate("jwt", { session: false }), async (req, res) => {
+app.get("/movies", async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
